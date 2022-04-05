83c9c9a4900d00dbabe9a20a1c1e67ac217756e9
EcCo18 <clemens.kurzeya@icloud.com>
2022-04-05 12:33:06 +0200

    feat(UserMapper): added debug messages

--- src/main/java/com/example/userservice/mapper/UserMapper.java
+++ src/main/java/com/example/userservice/mapper/UserMapper.java
@@ -2,14 +2,18 @@ package com.example.userservice.mapper;
 
 import com.example.userservice.model.User;
 import com.example.userservice.model.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserMapper {
 
     public UserDto mapUserToDto(User user) {
+        log.debug("mapping user to dto: " + user);
         return UserDto.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -18,10 +22,12 @@ public class UserMapper {
     }
 
     public List<UserDto> mapUserListToDtoList(List<User> userList) {
+        log.debug("mapping userList to dtoList: " + Arrays.toString(userList.toArray()));
         return userList.stream().map(this::mapUserToDto).toList();
     }
 
     public User mapDtoToUser(UserDto userDto) {
+        log.debug("mapping dto to user: " + userDto);
         return User.builder()
                 .id(userDto.getId())
                 .name(userDto.getName())
@@ -30,6 +36,7 @@ public class UserMapper {
     }
 
     public List<User> mapDtoListToUserList(List<UserDto> userDtoList) {
+        log.debug("mapping dtoList to userList: " + Arrays.toString(userDtoList.toArray()));
         return userDtoList.stream().map(this::mapDtoToUser).toList();
     }
 }
