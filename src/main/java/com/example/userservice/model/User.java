05314ba00e86c44d78e9b4a4b334d07cfe0a3261
EcCo18 <clemens.kurzeya@icloud.com>
2022-04-05 12:45:22 +0200

    fix(userEntity): added generated to the id

--- src/main/java/com/example/userservice/model/User.java
+++ src/main/java/com/example/userservice/model/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class User {
     @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String lastName;
