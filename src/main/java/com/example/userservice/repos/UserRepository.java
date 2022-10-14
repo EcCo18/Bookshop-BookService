982256b1a8a06d95b98ce3cb242ad6603a7486e8
EcCo18 <clemens.kurzeya@icloud.com>
2022-10-14 19:02:26 +0200

    refactor(user-repository): changed type to optional

--- src/main/java/com/example/userservice/repos/UserRepository.java
+++ src/main/java/com/example/userservice/repos/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.userservice.repos;
 import com.example.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByUsername(String username);
+    Optional<User> findUserByUsername(String username);
 }
